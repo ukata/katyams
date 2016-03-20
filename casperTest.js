dca19f7daccf73fd12b1d0f8ce4a5e8a3f73e6f9
Rafal Zawadzki <bluszcz@bluszcz.net>
2016-03-20 07:52:25 +0100

    fix casper

--- casperTest.js
+++ casperTest.js
@@ -1,5 +1,13 @@
+var casper = require('casper').create({
+      viewportSize: {width: 1920, height: 1080}
+});
+
 casper.start('https://ukata.me/', function() {
-            this.capture('ukata.png', {});
-            });
+    this.captureSelector('ukata.png', 'body')});
+
+casper.start('https://ukata.me/contact/', function() {
+    this.captureSelector('contact.png', 'body')});
+
+
 
 casper.run();
